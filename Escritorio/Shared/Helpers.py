e1c86c5a7bb8383950d77daf695ba1c7f5729209
kevinalexdev <kevinalexpe@gmail.com>
2021-11-18 00:04:42 -0500

    Formulario Productos

--- Escritorio/Shared/Helpers.py
+++ Escritorio/Shared/Helpers.py
@@ -1,7 +1,9 @@
 from tkinter import  Label
 
+from Shared.Constants import Constants
+
 class Controls:
 
     @staticmethod
     def colocarTitulo(frame, titulo):
-        Label(frame, text = titulo, bg ='white', font=('Arial',16,'bold')).grid(column =0, row=0, sticky='w')
+        Label(frame, text = titulo, bg =Constants.getBgColor(), font=('Arial',16,'bold')).grid(column =0, row=0, sticky='w', pady=10, padx=10)
